9a98f34900a13558011178a897cd720ac44d5c69
venturecandice <tido.squat@gmail.com>
2020-09-04 19:24:13 +0200

    mise en ligne du site

--- app/res.js
+++ app/res.js
@@ -18,14 +18,14 @@ class dog {
 
 let okkaina = new dog ("Okkaina", "10 Septembre 2018", caneCorso, templeJade, " ♀ femelle", false);
 let panama = new dog ("Panama", "02 Juin 2019", caneCorso, templeJade, " ♀ femelle", false);
-let rock = new dog ("Rock", "18 Mars 2018", caneCorso, diMunteanu, " ♂ mâle", true);
+let rock = new dog ("Rock", "18 Mars 2018", caneCorso, diMunteanu, " ♂ mâle", false);
 
 let dogClass = [okkaina, panama, rock];
 let femaleClass = [okkaina, panama];
 let maleClass = [rock];
 
 let path = "../src/img/";
-let images = ["okkaina1", "okkaina2", "okkaina3", 
+let images = ["okkaina1", "okkaina2", "okkaina3",
 "okkaina4", "okkaina5", "okkaina6", "okkaina7", "okkaina8", //"okkaina9",
 "okkaina10", "okkaina11",
 "panama1", "panama2","panama3", "panama4", "panama5", "panama6", 
