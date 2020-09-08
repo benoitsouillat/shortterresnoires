555be997a15257cce5d9b727ea17b0366204f724
venturecandice <tido.squat@gmail.com>
2020-09-08 11:23:47 +0200

    sass main done - a finir version smart

--- app/news.js
+++ app/news.js
@@ -8,30 +8,30 @@ articles.forEach(elm => {
 
     let asideArticle = document.createElement("aside");
         asideArticle.id = elm.artId;
-        asideArticle.classList.add("article-prev")
+        asideArticle.classList.add("article")
 
     let headArticle = document.createElement("div");
-        headArticle.classList.add("article-head");
+        headArticle.classList.add("article__head");
     let titleArticle = document.createElement("h2");
         titleArticle.textContent = elm.artTitle;
-        titleArticle.classList.add("article-title");
+        titleArticle.classList.add("article__title");
 
     let contentArticle = document.createElement("div");
-        contentArticle.classList.add("article-content");
+        contentArticle.classList.add("article__content");
     let descArticle = document.createElement("p");
         descArticle.innerText = elm.artDescription;
-        descArticle.classList.add("article-description");
+        descArticle.classList.add("article__description");
     let dateArticle = document.createElement("p");
         dateArticle.textContent = "le : " + elm.artDate;
-        dateArticle.classList.add("article-date");
+        dateArticle.classList.add("article__date");
 
     let footArticle = document.createElement("div");
-        footArticle.classList.add("article-foot");
+        footArticle.classList.add("article__foot");
     let authorArticle = document.createElement("strong");
         authorArticle.textContent = elm.artAuthor;
-        authorArticle.classList.add("article-author");
+        authorArticle.classList.add("article__author");
     let imageArticle = document.createElement("img");
-        imageArticle.classList.add("article-image");
+        imageArticle.classList.add("article__image");
         imageArticle.src = path + elm.artDog.dogName.toLowerCase() + "2"+ jpg;
 
 
