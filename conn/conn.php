a612e9bb525f1c54755b8a886f7040e8d03134ab
benoitsouillat <tido.squat@gmail.com>
2023-12-19 19:28:16 +0100

    prepare classes on pc to push on git

---
+++ conn/conn.php
@@ -0,0 +1,12 @@
+<?php
+
+$dsn = "mysql:host=localhost;port=3306;dbname=terresnoires";
+$username = "root";
+$password = "";
+
+try {
+    $conn = new PDO($dsn, $username, $password);
+    echo 'Connecté';
+} catch (PDOException $e) {
+    echo "Une erreur s'est produite : " . $e;
+}
