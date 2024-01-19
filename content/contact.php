0fe0d53b609d1f1f8a2da7b26d6eb7e5f4bb0fa6
Benoit Souillat <dev.souillat@gmail.com>
2024-01-19 11:51:36 +0100

    delete images useless

--- content/contact.php
+++ content/contact.php
@@ -48,10 +48,12 @@
             </div>
             <div>
                 <a href="tel:+33670378113" class="btn btn-dark"><span class="fa fa-phone"></span> Appelez-nous </a>
-                <a href="https://www.facebook.com/domaineterresnoires" target="blank_" class="btn btn-dark"><span class="fa fa-facebook-square"></span> Facebook </a>
+                <a href="https://www.facebook.com/domaineterresnoires" target="blank_" class="btn btn-dark"><span
+                        class="fa fa-facebook-square"></span> Facebook </a>
             </div>
             <div>
-                <a href="mailto:domainedesterresnoires@gmail.com" class="btn btn-dark"><span class="fa fa-envelope"></span> Email </a>
+                <a href="mailto:domainedesterresnoires@gmail.com" class="btn btn-dark"><span
+                        class="fa fa-envelope"></span> Email </a>
                 <a href="#contact-map" class="btn btn-dark"><span class="fa fa-map-marker"></span> Où sommes-nous </a>
             </div>
             <div>
@@ -62,9 +64,11 @@
 
         </section>
         <div id="contact-map">
-            <img src="/src/img/okkaina1-puppies/jaune/Image00020.jpg" alt="contactez-nous le domaine des terres noires" />
+            <img src="/src/img/Image00020.jpg" alt="contactez-nous le domaine des terres noires" />
 
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2802.229167721297!2d1.5572214!3d45.3845478!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f8e9c1848c5d45%3A0x44fcb4fd120ac343!2sLa%20Maison%20Neuve%2C%2019410%20Vigeois!5e0!3m2!1sfr!2sfr!4v1681554502521!5m2!1sfr!2sfr" loading="lazy"></iframe>
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2802.229167721297!2d1.5572214!3d45.3845478!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f8e9c1848c5d45%3A0x44fcb4fd120ac343!2sLa%20Maison%20Neuve%2C%2019410%20Vigeois!5e0!3m2!1sfr!2sfr!4v1681554502521!5m2!1sfr!2sfr"
+                loading="lazy"></iframe>
         </div>
         <br />
         <section id="adress-friend">
